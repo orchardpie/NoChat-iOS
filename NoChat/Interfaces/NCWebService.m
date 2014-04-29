69a64baa0e5e52a2d72d3c49016150b8819a89a6
Adam Milligan & Kevin Wolkober <pair+am+kw@orchardpie.com>
2014-04-29 17:32:25 -0400

    Set localized information correctly on 500 error

--- NoChat/Interfaces/NCWebService.m
+++ NoChat/Interfaces/NCWebService.m
@@ -139,7 +139,7 @@ typedef void(^AFFailureBlock)(NSURLSessionDataTask *task, NSError *error);
             default: {
                 NSError *anError = [NSError errorWithDomain:@"com.nochat.mobile"
                                                        code:0
-                                                   userInfo:@{ @"message": @"A thousand apologies, but an error occurred" }];
+                                                   userInfo:@{ NSLocalizedRecoverySuggestionErrorKey: @"A thousand apologies, but an error occurred" }];
                 error(anError);
             }
                 break;
